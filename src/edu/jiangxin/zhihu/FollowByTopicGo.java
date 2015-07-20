46b6786abfc82898a140eab5e9d93a999d104039
jiangxincode <jiangxinnju@163.com>
2015-07-20 19:43:00 +0800

    add FollowByTopciGo.java

--- src/edu/jiangxin/zhihu/FollowByTopicGo.java
+++ src/edu/jiangxin/zhihu/FollowByTopicGo.java
@@ -12,61 +12,51 @@ import org.openqa.selenium.interactions.HasInputDevices;
 public class FollowByTopicGo {
 
 	public static void main(String[] args) {
+		int followCount = 0;
 		WebDriver driver = new FirefoxDriver();
 		driver.get("http://www.zhihu.com");
-		
+
 		driver.findElement(By.linkText("µÇÂ¼")).click();
 		driver.findElement(By.name("account")).sendKeys(args[0]);
 		driver.findElement(By.name("password")).sendKeys(args[1]);
-		if(driver.findElement(By.name("remember_me")).isSelected()) {
+		if (driver.findElement(By.name("remember_me")).isSelected()) {
 			driver.findElement(By.name("remember_me")).click();
 		}
 		driver.findElement(By.className("sign-button")).click();
-		
+
 		try {
 			Thread.sleep(10000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		driver.get("http://www.zhihu.com/topic/19574589/followers");
-		List<WebElement> a = driver.findElements(By.tagName("a"));
-		while(true) {
-			for(WebElement we : a) {
-				if(we.getAttribute("class").contains("zg-btn-follow")) {
-					System.out.println(we.getAttribute("id"));
-					we.click();
-				}
+
+		driver.get("http://www.zhihu.com/topic/19674381/followers");
+		List<WebElement> a = driver.findElements(By.className("zg-btn-follow"));
+
+		for (WebElement we : a) {
+			we.click();
+			followCount++;
+		}
+		while (true) {
+			WebElement loadMore = driver.findElement(By.id("zh-load-more"));
+			if ((loadMore != null) && (loadMore.isDisplayed())) {
+				driver.findElement(By.id("zh-load-more")).click();
 			}
 			((HasInputDevices) driver).getKeyboard().sendKeys(Keys.PAGE_DOWN);
-			boolean has = false;
-			a = driver.findElements(By.tagName("a"));
-			for(WebElement we : a) {
-				if(we.getAttribute("class").contains("zg-btn-follow")) {
-					has = true;
-				}
-			}
-			while(!has) {
-				if(driver.findElement(By.id("zh-load-more")) != null) {
-					driver.findElement(By.id("zh-load-more")).click();
-				}
-				((HasInputDevices) driver).getKeyboard().sendKeys(Keys.PAGE_DOWN);
-				a = driver.findElements(By.tagName("a"));
-				for(WebElement we : a) {
-					if(we.getAttribute("class").contains("zg-btn-follow")) {
-						has = true;
-					}
+			a = driver.findElements(By.className("zg-btn-follow"));
+			for (WebElement we : a) {
+				we.click();
+				followCount++;
+				if(followCount == 1413) {
+					System.exit(1);
 				}
 			}
 		}
-		
-		
-		//File cookieFile = new File("cookie.txt");
-		
-
-		//driver.close();
-
 	}
+
+	// File cookieFile = new File("cookie.txt");
+
+	// driver.close();
 
 }
