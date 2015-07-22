0288012fc603344ddef694904231808c963d8b0c
jiangxincode <jiangxinnju@163.com>
2015-07-22 23:13:34 +0800

    merge some follow function into one module

--- src/edu/jiangxin/zhihu/FollowByTopic.java
+++ src/edu/jiangxin/zhihu/FollowByTopic.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Cookie;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -25,15 +26,33 @@ public class FollowByTopic {
 			driver.manage().addCookie(cookie);
 		}
 
-		driver.get("http://www.zhihu.com/topic/19627873/followers");
+		//driver.get("http://www.zhihu.com/topic/19627873/followers"); //按照话题进行关注
+		//driver.get("http://www.zhihu.com/question/19644659/followers"); //按照问题进行关注
+		//driver.get("http://www.zhihu.com/people/YenvY/followees"); //某人关注的人
+		driver.get("http://www.zhihu.com/people/YenvY/followers"); //关注某人的人
 		
 		
 		List<WebElement> follow = driver.findElements(By.className("zg-btn-follow"));
 		List<WebElement> unfollow = driver.findElements(By.className("zg-btn-unfollow"));
-		String followNumber = driver.findElement(By.tagName("strong")).getText();
-		System.out.println(followNumber);
-		while(follow.size() + unfollow.size() < Integer.parseInt(followNumber)-1) { // in case of the ower of the program has followed this topic
-			WebElement loadMore = driver.findElement(By.id("zh-load-more"));
+		
+		int duplicate = 0;
+		int sum_follow_unfollow = follow.size() + unfollow.size();
+		
+		/**
+		 * 如果下拉三次滚动条，没有变化就停止循环，该方法不用定位页面上的一些参考值（这些值不太好定位，而且适应情况有限，而且对于程序执行者是否关注需要分情况讨论）
+		 * 曾经的方法是：
+		 * String followNumber = driver.findElement(By.tagName("strong")).getText(); //定位总人数
+		 * while(follow.size() + unfollow.size() < Integer.parseInt(followNumber)-1) { //程序执行者可能在关注者之中
+		 */
+		while(duplicate < 3) {
+			
+			WebElement loadMore;
+			
+			try {
+				loadMore = driver.findElement(By.id("zh-load-more")); //如果不存在该元素会抛出异常，而不是直接返回null
+			} catch (NoSuchElementException e) {
+				loadMore = null;
+			}
 			if((loadMore != null) && loadMore.isDisplayed()) {
 				loadMore.click();
 			} else {
@@ -43,7 +62,13 @@ public class FollowByTopic {
 			}
 			follow = driver.findElements(By.className("zg-btn-follow"));
 			unfollow = driver.findElements(By.className("zg-btn-unfollow"));
-			System.out.println(follow.size() + " " + unfollow.size());
+			if(follow.size() + unfollow.size() > sum_follow_unfollow) {
+				duplicate = 0;
+				sum_follow_unfollow = follow.size() + unfollow.size();
+			} else {
+				duplicate++;
+			}
+			System.out.println(follow.size() + " " + unfollow.size() + " " + duplicate);
 		}
 		
 		for(WebElement we : follow) {
