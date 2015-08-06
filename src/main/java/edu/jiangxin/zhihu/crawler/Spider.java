5ef68a9c50b0ff645a961b36c3d11c5a736b8a54
jiangxincode <jiangxinnju@163.com>
2015-08-06 19:02:59 +0800

    temp

---
+++ src/main/java/edu/jiangxin/zhihu/crawler/Spider.java
@@ -0,0 +1,74 @@
+package edu.jiangxin.zhihu.crawler;
+
+import java.util.ArrayList;
+
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class Spider {
+	public static void main(String[] args) {
+
+		String url = "http://www.zhihu.com/explore/recommendations";
+		// 获取知乎推荐首页
+		String content = Spider.SendGet(url);
+
+		// 获取推荐内容详细内容
+		ArrayList<Zhihu> myZhihu = Spider.GetRecommendations(content);
+
+		// 写入文档
+		for (Zhihu zhihu : myZhihu) {
+			FileReaderWriter.writeIntoFile(zhihu.writeString(),"D:/知乎_编辑推荐.txt", true);
+		}
+	}
+	
+	//获取指定Url页面内容
+	public static String SendGet(String url) {
+
+		CloseableHttpClient client = HttpClients.createDefault();
+		try{
+			HttpGet request = new HttpGet(url);
+			CloseableHttpResponse resp = client.execute(request);
+			
+			String result = EntityUtils.toString(resp.getEntity());
+			
+			return result;
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			try{
+				client.close();
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+			
+		}
+		
+		return null;
+	}
+
+	// 获取推荐内容详细内容url
+	public static ArrayList<Zhihu> GetRecommendations(String content) {
+		
+		ArrayList<Zhihu> results = new ArrayList<Zhihu>();
+		Document doc = Jsoup.parse(content);
+		Elements items =  doc.getElementsByClass("zm-item");          //推荐内容元素
+		for(Element item:items){
+			Element h2TagEle = item.getElementsByTag("h2").first();   //推荐内容标题元素
+			Element aTagEl = h2TagEle.getElementsByTag("a").first();  //推荐内容的Url超链接元素
+			String href = aTagEl.attr("href");                        //推荐内容url
+			if(href.contains("question")){                            //去除不规范url
+				results.add(new Zhihu(href));
+			}
+		}
+		return results;
+	}
+
+}
+
---
+++ src/main/java/edu/jiangxin/zhihu/crawler/Zhihu.java
@@ -0,0 +1,93 @@
+package edu.jiangxin.zhihu.crawler;
+
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class Zhihu {
+	public String question;// 问题
+	public String questionDescription;// 问题描述
+	public String zhihuUrl;// 链接地址
+	public ArrayList<String> answers;// 回答
+
+	// 获取知乎内容
+	public Zhihu(String url) {
+		question = "";
+		questionDescription = "";
+		zhihuUrl = "";
+		answers = new ArrayList<String>();
+
+		if (getRealUrl(url)) {
+			
+			// 获取推荐内容详细页面
+			String content = Spider.SendGet(zhihuUrl);
+			if(content != null){
+				Document doc = Jsoup.parse(content);
+				// 获取标题，即用户发布的问题
+				question = doc.title();
+				
+				// 问题消息标书
+				Element despElement = doc.getElementById("zh-question-detail");
+				if(despElement != null){
+					questionDescription = despElement.text();
+				}
+				// 解答
+				Elements ansItems = doc.getElementsByClass("zm-item-answer");
+				for(Element ansItem:ansItems){
+					Element textElement = ansItem.getElementsByClass("zm-item-rich-text").first();
+					if(despElement != null){
+						answers.add(textElement.text());
+					}
+				}
+			}else{
+				System.out.println("content is null");
+			}
+		}
+	}
+
+	// 
+	boolean getRealUrl(String url) {
+		// 将http://www.zhihu.com/question/22355264/answer/21102139
+		// 转换为http://www.zhihu.com/question/22355264
+		Pattern pattern = Pattern.compile("question/(.*?)/");
+		Matcher matcher = pattern.matcher(url);
+		if (matcher.find()) {
+			zhihuUrl = "http://www.zhihu.com/question/" + matcher.group(1);
+		} else {
+			return false;
+		}
+		return true;
+	}
+
+	public String writeString() {
+		// 将html页面转换为字符串
+		String result = "";
+		result += "问题：" + question + "\r\n";
+		result += "描述：" + questionDescription + "\r\n";
+		result += "链接：" + zhihuUrl + "\r\n\r\n";
+		for (int i = 0; i < answers.size(); i++) {
+			result += "回答" + i + "：" + answers.get(i) + "\r\n\r\n\r\n";
+		}
+		result += "\r\n\r\n\r\n\r\n\r\n\r\n";
+		// 替换html换行符和其他字符
+		result = result.replaceAll("<br>", "\r\n");
+		result = result.replaceAll("<.*?>", "");
+		return result;
+	}
+
+	@Override
+	public String toString() {
+		String result = "";
+		result += "问题：" + question + "\n";
+		result += "描述：" + questionDescription + "\n";
+		result += "链接：" + zhihuUrl + "\n";
+		result += "回答：" + answers.size() + "\n";
+		return result;
+	}
+}
+
