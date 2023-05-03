49875a5a864e08e5da526ff51ab2ba8cc8bf5169
Aloys <jiangxinnju@163.com>
2023-05-03 12:42:14 +0800

    code optimization

--- src/main/java/edu/jiangxin/zhihu/crawler/HttpClientTest.java
+++ src/main/java/edu/jiangxin/zhihu/crawler/HttpClientTest.java
@@ -17,12 +17,9 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
- 
-/**
- * Created by gavin on 15-7-23.
- */
+
 public class HttpClientTest {
- 
+
     public static void main(String[] args)
     {
         //创建一个HttpClient
@@ -38,7 +35,7 @@ public class HttpClientTest {
             String xsrfValue = responseHtml.split("<input type=\"hidden\" name=\"_xsrf\" value=\"")[1].split("\"/>")[0];
             System.out.println("xsrfValue:" + xsrfValue);
             response.close();
-             
+
             //构造post数据
             List<NameValuePair> valuePairs = new LinkedList<NameValuePair>();
             valuePairs.add(new BasicNameValuePair("_xsrf", xsrfValue));
@@ -46,17 +43,17 @@ public class HttpClientTest {
             valuePairs.add(new BasicNameValuePair("password", "621224jx"));
             valuePairs.add(new BasicNameValuePair("remember_me", "true"));
             UrlEncodedFormEntity entity = new UrlEncodedFormEntity(valuePairs, Consts.UTF_8);
-             
+
             //创建一个post请求
             HttpPost post = new HttpPost("http://www.zhihu.com/login/email");
             post.setHeader("Cookie", " cap_id=\"YjA5MjE0YzYyNGQ2NDY5NWJhMmFhN2YyY2EwODIwZjQ=|1437610072|e7cc307c0d2fe2ee84fd3ceb7f83d298156e37e0\"; ");
- 
+
             //注入post数据
             post.setEntity(entity);
             HttpResponse httpResponse = httpClient.execute(post);
             //打印登录是否成功信息
             printResponse(httpResponse);
- 
+
             //构造一个get请求，用来测试登录cookie是否拿到
             HttpGet g = new HttpGet("http://www.zhihu.com/question/followers");
             //得到post请求返回的cookie信息
@@ -77,7 +74,7 @@ public class HttpClientTest {
             }
         }
     }
- 
+
     public static void printResponse(HttpResponse httpResponse)
             throws ParseException, IOException {
         // 获取响应消息实体
@@ -97,7 +94,7 @@ public class HttpClientTest {
                     + responseString.replace("\r\n", ""));
         }
     }
- 
+
     public static Map<String,String> cookieMap = new HashMap<String, String>(64);
     //从响应信息中获取cookie
     public static String setCookie(HttpResponse httpResponse)
@@ -117,7 +114,7 @@ public class HttpClientTest {
                 cookie += ";";
             }
         }
- 
+
         String cookies[] = cookie.split(";");
         for (String c : cookies)
         {
@@ -134,7 +131,7 @@ public class HttpClientTest {
         {
             cookiesTmp +=key+"="+cookieMap.get(key)+";";
         }
- 
+
         return cookiesTmp.substring(0,cookiesTmp.length()-2);
     }
 }
