ed14f87bf43cb591ff3be2bd169428b1d7e0a4f3
Aloys <jiangxinnju@163.com>
2025-09-21 08:36:33 +0800

    fix compile errors because of bump of selenium

--- src/main/java/edu/jiangxin/zhihu/core/Follow.java
+++ src/main/java/edu/jiangxin/zhihu/core/Follow.java
@@ -13,7 +13,7 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.internal.Locatable;
+import org.openqa.selenium.interactions.Locatable;
 import org.openqa.selenium.support.FindBy;
 
 public class Follow {
